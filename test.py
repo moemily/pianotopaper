27b3083561bc3a5bc6a6e74149f731a5c2b041c9
oliverdmiles <omiles@umich.edu>
2020-11-17 19:29:16 -0500

    test push

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 import numpy as np
 import cv2
 import sys, os 
+import matplotlip.pyplot as plt
+import matplotlib.lines as mlines
 
 print("Hi")
 
@@ -20,11 +22,17 @@ while (fc < frame_cnt and ret):
 gi = cv2.cvtColor(buf[4], cv2.COLOR_BGR2GRAY)
 bi = cv2.GaussianBlur(gi, (7,7,), .2)
 med_val = np.median(bi)
-icannybelieveit = cv2.Canny(bi, 0, 255)
+icannybelieveit = cv2.Canny(bi, 128, int(max(0, 0.7*med_val)), int(min(255, 1.3*med_val)))
 
 
 cap.release()
 cv2.namedWindow('frame 10')
-cv2.imshow('frame 10', gi)
 cv2.imshow('frame 10', icannybelieveit)
-#input("Press enter to fuck off")
+input("Press enter to fuck off")
+
+def hough_line(im, edge_im, num_rhos, num_thetas, t_count):
+    N, M = edge_im.shape[:2]
+    Nhalf, Mhalf = N/2 , M/2
+    d = np.sqrt(np.square(N) + np.square(M))
+    dtheta = 180 / num_thetas
+    drho = (2*d) / num_rhos
