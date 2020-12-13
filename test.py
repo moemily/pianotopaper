12c4ac1ba1cb8e3d6a8c260bcda6ddb53c944e90
oliverdmiles <omiles@umich.edu>
2020-12-12 20:35:11 -0500

    some new stuff

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 import numpy as np
 import cv2
 import sys, os 
@@ -11,22 +11,53 @@ from skimage import segmentation
 from skimage import io, color
 import skimage
 from skimage.measure import label
+from PIL import Image
 
 WIDTH = 1280
 HEIGHT = 720
-BG_WIDTH = 1342
+BG_WIDTH = 1368
 BG_HEIGHT = 270
 BG_IMAGE = None
+BG_SEG = None
+BG_TEMP = None
 BG_MEAN = 0
 BG_KEYS = 0
+CROP_AMOUNT = 5
+
+
+def show_lines(img, lines):
+    for line in lines:
+        for rho, theta in line:
+            delta_v = np.pi/float(32)
+            delta_h = np.pi/float(128)
+            diff_h = abs(theta - 3*np.pi/2) if theta > np.pi else abs(theta - np.pi/2)
+            diff_v = abs(theta - np.pi) if theta > np.pi/2 else abs(theta)
+            if diff_h > delta_h and diff_v > delta_v:
+                continue
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 200*(-b))
+            y1 = int(y0 + 200*(a))
+            x2 = int(x0 - 200*(-b))
+            y2 = int(y0 - 200*(a))
+            cv2.line(img, (x1,y1), (x2,y2), (0,0,255),2)
+
+def show_intersections(img, intersections, N):
+    for i in range(0, N):
+        img[int(intersections[i][0]), int(intersections[i][1]), 0] = 0
+        img[int(intersections[i][0]), int(intersections[i][1]), 1] = 255
+        img[int(intersections[i][0]), int(intersections[i][1]), 2] = 0
+    cv2.imshow("intersections", img)
 
 '''
 in:
     lines - N x 1 X 2 list of lines in our image, (rho, theta)
 out: 
-    line1 - N x 4 numpyarray that is line 1 to make the intersetction, (x1,y1,x2,y2)
-    line2 - N X 4 numpyarray that is line 2 to make the intersetction, (x1,y1,x2,y2)
-    intersect - N x 2 numpyarry that is the set of unique intersection points of line 1 and line 2 (x, y)
+    line1 - N x 4 list that is line 1 to make the intersetction, (x1,y1,x2,y2)
+    line2 - N X 4 list that is line 2 to make the intersetction, (x1,y1,x2,y2)
+    intersect - N x 2 list that is the set of unique intersection points of line 1 and line 2 (x, y)
 '''
 def find_intersections(lines):
     hvlines = []
@@ -105,6 +136,10 @@ out:
   boxes - M x 4 X 2  numpyarray that is a list of M sets of 4 lines that intersect to form a box, (x,y)
 '''
 def find_boxes(line1, line2, intersect) :
+
+    # want map of intersections -> line and line -> intersection 
+
+
     same_line = [] # intersections on the same line, each entry i holds ((intersections), line of intersection, (non intersecting lines))
     boxes = []
     iteration = 0
@@ -214,6 +249,36 @@ def illumination_normalization(bg, frame, step=255) :
     norm = frame + np.minimum(abs(bg - frame),step) * np.sign(bg - frame)
     return norm
 
+'''
+in:
+    bg: background frame
+    frame: current frame (hands)
+out: 
+    posbin: binary image of positive parts of norm - bg
+    negbin: binary image of negative parts of norm - bg
+'''
+def bg_subtraction(bg, frame) :
+    bg_gray = cv2.cvtColor(bg, cv2.COLOR_RGB2GRAY)
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    norm = illumination_normalization(bg_gray, frame_gray, 100) #100 is magic number
+    
+    pos = norm - bg_gray #black keys
+    pos = cv2.GaussianBlur(pos, (15,15,), .2) #magic number kernals
+    thresh = 200 #also magic number thresholds
+    posbin = pos 
+    posbin[pos>=thresh] = 255
+    posbin[pos<thresh] = 0
+
+    neg = bg_gray - norm #white keys
+    neg = cv2.GaussianBlur(neg, (15,15,), .2)
+    thresh = 70
+    negbin = neg
+    negbin[neg >= thresh] = 255
+    negbin[neg < thresh] = 0
+
+    return posbin,negbin
+
+    
 index = 0
 # IN:
 #   takes a candidate background image and checks if it's a keyboard
@@ -231,10 +296,9 @@ def test_if_image_is_keyboard(candidate):
     if not cond1: 
         #debug information
         #print("Lower third was not brighter than top two thirds")
-        return 0, 0
+        return 0, 0, 0
     #magic numbers 170/255
     #magic number crop amount 
-    CROP_AMOUNT = 5
     _, binary = cv2.threshold(blurred_image[2*CROP_AMOUNT:-2*CROP_AMOUNT,CROP_AMOUNT:-CROP_AMOUNT],170,255,cv2.THRESH_BINARY)
     img = label(binary)
     num_labels = len(np.unique(img))
@@ -262,7 +326,7 @@ def test_if_image_is_keyboard(candidate):
     # img[img != 0] = 255
     img = skimage.img_as_ubyte(img) 
     num_keys = len(np.unique(img)) - 1
-
+    
     #this shows the keys / regions clearly 
     # global index
     # img[img != 0] = 255
@@ -274,82 +338,148 @@ def test_if_image_is_keyboard(candidate):
     if not cond2:
         #debug information
         #print("Found ", num_keys, " black keys")
-        return 0, 0
+        return 0, 0, 0
 
     if cond1 and cond2: 
-        return brightness_bottom, num_keys
-    return 0, 0
+        seg_img = np.zeros((img.shape[0]+4*CROP_AMOUNT,img.shape[1]+2*CROP_AMOUNT)).astype('uint8')
+        seg_img[2*CROP_AMOUNT:-2*CROP_AMOUNT,CROP_AMOUNT:-CROP_AMOUNT] = img
+        return brightness_bottom, num_keys, seg_img
+    return 0, 0, 0
 
 # IN:
 #   takes image and coorindates of a box 
 # OUT:
 #   returns warped image with dimensions BG_{WIDTH, HEIGHT} from image
+#currently depends on TL - BL - BR - TR order of points
 def get_homography_image(frame, box):
     background = np.array([[0, 0],[0, BG_HEIGHT],[BG_WIDTH, BG_HEIGHT],[BG_WIDTH, 0]])
     homography, status = cv2.findHomography(box, background)
     return cv2.warpPerspective(frame, homography, (BG_WIDTH, BG_HEIGHT))
+
+def sort_coords(sub_list):
+    sub_list.sort(key = lambda x: x[1])
+
+#sorts into tl bl br tr
+def sort_box_coordinates(box):   
+    y1 = None
+    y2 = None
+    cur_min_y = BG_WIDTH
+    sec_min_y = BG_WIDTH
+    for i in range(4):
+        if box[i][1] < cur_min_y:
+            sec_min_y = cur_min_y
+            y1 = y2
+            cur_min_y = box[i][1]
+            y2 = i
+        elif box[i][1] < sec_min_y:
+            sec_min_y = box[i][1]
+            y1 = i
+    one = None
+    two = None
+    three = None
+    four = None
+    indicies = [0, 1, 2, 3]
+    print(indicies)
+    if (box[y1][0] < box[y2][0]):
+        one = y1
+        two = y2
+    else:
+        one = y2
+        two = y1
+    indicies.remove(one)
+    indicies.remove(two)
+    print(box.shape)
+    if box[indicies[0]][0] > box[indicies[1]][0]:
+        three = indicies[0]
+        four = indicies[1]
+    else:
+        three = indicies[1]
+        four = indicies[0]
+    
+    sorted_box = np.zeros_like(box)
+    sorted_box[0] = box[one]
+    sorted_box[1] = box[two]
+    sorted_box[2] = box[three]
+    sorted_box[3] = box[four]
+    return sorted_box
 
 # in:
 #   candidate background image and array of potential boxes
 # out:
 #   bool whether it updated BG_IMAGE or not indicating, essentially, whether BG_IMAGE is valid or not
 def get_background_image(frame, boxes):
-    global BG_IMAGE, BG_MEAN, BG_KEYS
-    best_mean = 0
-    best_keys = 0
+    global BG_IMAGE, BG_MEAN, BG_KEYS, BG_SEG
     updated_background = False
     for box in boxes:
+        sorted_box = sort_box_coordinates(box)
+        print("box:", box)
+        print("sorted_box: ", sorted_box)
         candidate = get_homography_image(frame, box)
-        test_mean, test_keys = test_if_image_is_keyboard(candidate)
+        test_mean, test_keys, test_seg = test_if_image_is_keyboard(candidate)
         if test_mean > BG_MEAN and test_keys >= BG_KEYS:
             BG_MEAN = test_mean
             BG_KEYS = test_keys
             BG_IMAGE = candidate
+            BG_SEG = test_seg
             updated_background = True
+            print(box)
     return updated_background
 
 def find_lines(frame) :
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(grey, (7,7,), .2)
+    grey = cv2.cvtColor(frame[300:,:], cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(grey, (7,7,), .2) #magic number
 
     # Canny Edge Detection 
-    icannybelieveit = cv2.Canny(blurred, 0, 255, apertureSize=3)
-    
+    icannybelieveit = cv2.Canny(blurred, 0, 255, apertureSize=3) #magic number
     # Hough Line Detection  (1 and 150 are magic numbers)
-    lines = cv2.HoughLines(icannybelieveit, 1, np.pi/180, 150)
+    lines = cv2.HoughLines(icannybelieveit, 1, 1*np.pi/180, 100) #magic number
+    # show_lines(frame, lines)
+
+    # #magic numbers galore
+    # blur = cv2.GaussianBlur(grey, (1,1),0)
+    # _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    # mask = np.ones(grey.shape[:2], dtype="uint8")*255
+    # for c in contours:
+    #     x, y, w, h = cv2.boundingRect(c)
+    #     if w*h > 4000:
+    #         cv2.rectangle(mask, (x,y), (x+w, y+h), (0,0,255), -1)
+    # cv2.imshow("gray", grey)
+    # cv2.imshow("boxes", mask)
+
     return lines
 
-def show_lines_and_intersections(orig, lines, lines1, lines2, intersections, N):
-    cnt = 0
+def clean_segmentation(img):
+    start = None
+    end = None
+    for i in range(1,img.shape[1]):
+        if not img[2*CROP_AMOUNT,i-1] and img[2*CROP_AMOUNT,i]:
+            start = i
+        if img[2*CROP_AMOUNT,i-1] and not img[2*CROP_AMOUNT,i]:
+            end = i
+        if start and end:
+            img[:2*CROP_AMOUNT,start:end] = img[2*CROP_AMOUNT+1, start+1]
+            start = None
+            end = None
+    width = int(img.shape[1] / 36)
+    for i in range(36):
+        key = np.array(img[:,i*width:i*width + width])
+        key[img[:,i*width:i*width + width] == 0] = BG_KEYS + i + 1
+        img[:,i*width:i*width + width] = key
+        # show lines for white keys
+        # img[:,i*width+width:i*width+width+1] = 255
+    return img
 
-    for line in lines:
-        for rho, theta in line:
-            delta_v = np.pi/float(32)
-            delta_h = np.pi/float(128)
-            diff_h = abs(theta - 3*np.pi/2) if theta > np.pi else abs(theta - np.pi/2)
-            diff_v = abs(theta - np.pi) if theta > np.pi/2 else abs(theta)
-            if diff_h > delta_h and diff_v > delta_v:
-                continue
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 3000*(-b))
-            y1 = int(y0 + 3000*(a))
-            x2 = int(x0 - 3000*(-b))
-            y2 = int(y0 - 3000*(a))
-            cv2.line(orig, (x1,y1), (x2,y2), (0,0,255),2)
-        cnt += 1
-
-    for i in range(0, N):
-        orig[int(intersections[i][0]), int(intersections[i][1]), 0] = 0
-        orig[int(intersections[i][0]), int(intersections[i][1]), 1] = 255
-        orig[int(intersections[i][0]), int(intersections[i][1]), 2] = 0
-    cv2.imshow("intersections", orig)
+def get_keys(coords):
+    global BG_SEG
+    key_list = []
+    for i in coords:
+        key_list.append(BG_SEG[i[0],i[1]])
+    return key_list
 
 def do_stuff():
-    #cap = cv2.VideoCapture("00006.mp4")
-    cap = cv2.VideoCapture("videos/EveryNoteOneFinger.mp4")
+    cap = cv2.VideoCapture("videos/mov.mp4")
+    #cap = cv2.VideoCapture("videos/EveryNoteOneFinger.mp4")
     frame_cnt = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     global WIDTH
     global HEIGHT 
@@ -362,26 +492,48 @@ def do_stuff():
         ret, buf[fc] = cap.read()
         fc += 1
 
-    mycopy = copy.deepcopy(buf[0])
-    mycopy = mycopy[320:600,:]
-    orig = copy.deepcopy(mycopy)
-    cv2.imshow("cropped", mycopy)
-    print("Getting lines")
+    # mycopy = copy.deepcopy(buf[0]) #magic number
+    # mycopy = mycopy[320:600,:] #magic number (cropped example)
+    # orig = copy.deepcopy(mycopy)
+    # cv2.imshow("cropped", mycopy)
+    # print("Getting lines")
+    # lines = find_lines(orig)
+    # print("Calculating Intersections")
+    # lines1, lines2, intersect, N = find_intersections(lines)
+    # print("intersections is N is ", N)
+    # show_lines_and_intersections(mycopy, lines, lines1, lines2, intersect, N)
+    # print("Generating boxes")
+    # boxes = find_boxes(lines1, lines2, intersect)
+    # print("Getting Background Image")
+    # success = get_background_image(orig, boxes)
+    # print("Was a success: ", success)
+    # cv2.imshow("background", BG_IMAGE)
+
+    orig = copy.deepcopy(buf[0])
+    cv2.imwrite("bgcheck.jpg", orig)
+    #this one is for the everynote finger video
+    #boxes = np.array([[[46,374],[49,556],[1245,548],[1243,356]],[[16,3544],[19,526],[1215,518],[1213,326]]])
+    #trying to get a box on the tilted one to work
+    boxes = np.array([[[497,1196],[646,1240],[437,70],[257,130]],[[16,3544],[19,526],[1215,518],[1213,326]]])
+    cv2.imshow("homogaphy", get_homography_image(orig, boxes[0]))
+    bg = get_background_image(orig, boxes)
+    if (bg == False):
+        print("Was not able to find background from the boxes provided")
+    #bg = get_background_image(orig, boxes)
+    #box = np.array([[[46,374],[49,556],[1245,548],[1243,356]],[[16,3544],[19,526],[1215,518],[1213,326]]])
+    #boxes = np.array([[497,1196],[646,1240],[437,70],[257,130]])
+    #bg = get_background_image(orig, box)
+    #sort_box_coordinates(boxes[0])
+    print("we here")
     lines = find_lines(orig)
-    print("Calculating Intersections")
-    lines1, lines2, intersect, N = find_intersections(lines)
-    print("intersections is N is ", N)
-    show_lines_and_intersections(mycopy, lines, lines1, lines2, intersect, N)
-    print("Generating boxes")
-    boxes = find_boxes(lines1, lines2, intersect)
-    print("Getting Background Image")
-    success = get_background_image(orig, boxes)
-    print("Was a success: ", success)
+    show_lines(orig, lines)
+    _, _, intersections, N = find_intersections(lines)
+    show_intersections(orig, intersections, N)
     cv2.imshow("background", BG_IMAGE)
-
-    # orig = copy.deepcopy(buf[0])
-    # bg = get_background_image(orig, np.array([[[46,374],[49,556],[1245,548],[1243,356]],[[16,3544],[19,526],[1215,518],[1213,326]]]))
-
+    global BG_SEG
+    BG_SEG = clean_segmentation(BG_SEG)
+    #BG_SEG[BG_SEG == 26] = 255
+    cv2.imshow("segmentation", BG_SEG)
     cap.release()
 
     #emily stuff
